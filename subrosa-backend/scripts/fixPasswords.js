8bae49ba13edb72e1b4c2c874713f13c12c162a9
Benjamin Hoffelé <hofben@hotmail.fr>
2025-04-30 16:21:11 +0200

    Creation script pour modification des passwords en Bdd, car les premiers artistes n'avait pas de mot de passe hashé par bcrypt, script Ok , modif ok, Bdd ok

---
+++ subrosa-backend/scripts/fixPasswords.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Artist = require('../src/models/Artists');
+
+
+mongoose.connect(process.env.MONGO_URI); // adapte l’URL si besoin
+
+async function fixPlainPasswords() {
+  const artists = await Artist.find();
+
+  for (let artist of artists) {
+    // si le mot de passe n'est pas hashé (ex: < 60 caractères ou ne commence pas par $2b$)
+    if (!artist.password.startsWith('$2b$')) {
+      const hashed = await bcrypt.hash(artist.password, 10);
+      artist.password = hashed;
+      await artist.save();
+      console.log(`✅ Mot de passe hashé pour ${artist.username}`);
+    }
+  }
+
+  console.log('✔️ Tous les mots de passe sont maintenant hashés.');
+  process.exit();
+}
+
+fixPlainPasswords();
